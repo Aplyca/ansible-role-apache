8661293b906f644bb2e0b288a6c23db147d45001
Mauricio Sánchez <msanchez@aplyca.com>
2016-11-08 15:44:48 -0500

    Adding Apache3 PPA for latests versions

--- tests/test.sh
+++ tests/test.sh
@@ -4,11 +4,10 @@
 cd $(dirname "$0") && cd ..
 
 echo "Checking syntax"
-ansible-playbook -i inventories/local playbooks.yml --extra-vars="@tests/tests.yml" --connection=local --syntax-check
+ansible-playbook -i inventories/local playbooks.yml --extra-vars="@tests/test.yml" --connection=local --syntax-check
 
 echo "Running role"
-ansible-playbook -i inventories/local playbooks.yml --extra-vars="@tests/tests.yml" --connection=local
+ansible-playbook -i inventories/local playbooks.yml --extra-vars="@tests/test.yml" --connection=local
 
 echo "Checking idempotence"
-ansible-playbook -i inventories/local playbooks.yml --extra-vars="@tests/tests.yml" --connection=local | grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)
-
+ansible-playbook -i inventories/local playbooks.yml --extra-vars="@tests/test.yml" --connection=local | grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)
